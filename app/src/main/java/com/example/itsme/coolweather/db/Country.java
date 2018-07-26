3966908e17e2b00d8070c32b2bf61c61cf12ffb8
Your Name <you@example.com>
2018-07-26 16:15:14 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/itsme/coolweather/db/Country.java
@@ -0,0 +1,43 @@
+package com.example.itsme.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport{
+    private int id;
+    private String countryName;
+    private String weatherCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherCode() {
+        return weatherCode;
+    }
+
+    public void setWeatherCode(String weatherCode) {
+        this.weatherCode = weatherCode;
+    }
+
+}
